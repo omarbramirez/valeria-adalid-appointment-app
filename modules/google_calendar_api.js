5c4fc79376d11f56d25d5b8ddc5af509d493a4dd
Omar Ramírez <omar.ramirez94@hotmail.es>
2022-09-26 21:57:27 -0500

    __dirname changed

--- modules/google_calendar_api.js
+++ modules/google_calendar_api.js
@@ -97,8 +97,8 @@ exports.googleCalendarValidator = (event, startTime, endTime, response) => {
             brand_name: `Nutriologadalid.com`,
             landing_page: 'NO_PREFERENCE', // Default, para mas informacion https://developer.paypal.com/docs/api/orders/v2/#definition-order_application_context
             user_action: 'PAY_NOW', // Accion para que en paypal muestre el monto del pago
-            return_url: `http://localhost:3000/success`, // Url despues de realizar el pago
-            cancel_url: `http://localhost:3000/cancel-payment` // Url despues de realizar el pago
+            return_url: `${__dirname}/success`, // Url despues de realizar el pago
+            cancel_url: `${__dirname}/cancel-payment` // Url despues de realizar el pago
           }
         }
         //https://api-m.sandbox.paypal.com/v2/checkout/orders [POST]
