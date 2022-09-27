9b7e2b73e5f1008b4e5b45bfb3688b27b352251f
Omar Ramírez <omar.ramirez94@hotmail.es>
2022-09-27 17:00:04 -0500

    days array modified

--- modules/google_calendar_api.js
+++ modules/google_calendar_api.js
@@ -35,7 +35,7 @@ const calendar = google.calendar({
   version: 'v3', auth: oAuth2Client
 });
 
-var days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+var days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
 var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
 // IMPORT DATA TO GOOGLE CALENDAR
@@ -97,12 +97,12 @@ exports.googleCalendarValidator = (event, startTime, endTime, response) => {
             brand_name: `Nutriologadalid.com`,
             landing_page: 'NO_PREFERENCE', // Default, para mas informacion https://developer.paypal.com/docs/api/orders/v2/#definition-order_application_context
             user_action: 'PAY_NOW', // Accion para que en paypal muestre el monto del pago
-            /*
+
             return_url: `http://localhost:3000/success`, // Url despues de realizar el pago
             cancel_url: `http://localhost:3000/` // Url despues de realizar el pago
-            */
-            return_url: `https://valeria-adalid-appointment-app.herokuapp.com/success`, // Url despues de realizar el pago
-            cancel_url: `https://valeria-adalid-appointment-app.herokuapp.com/cancel-payment` // Url despues de realizar el pago
+
+            // return_url: `https://valeria-adalid-appointment-app.herokuapp.com/success`, // Url despues de realizar el pago
+            // cancel_url: `https://valeria-adalid-appointment-app.herokuapp.com/cancel-payment` // Url despues de realizar el pago
           }
         }
         //https://api-m.sandbox.paypal.com/v2/checkout/orders [POST]
